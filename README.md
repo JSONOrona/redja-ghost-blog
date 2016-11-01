c0dff704e29593512eb2a9b4148d11f796e24349
Jason Orona <jason.orona@theaiteam.coml>
2016-11-01 18:23:00 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# redja-ghost-blog
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: redja-ghost-blog
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
